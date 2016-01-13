023104ceec0ad1e5353ffea026cffef1cc3b38d6
Rayne <lukain337@gmail.com>
2016-01-13 19:54:07 +0900

    initial commit

--- Loops/ViewController.swift
+++ Loops/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var resultLabel1: UILabel!
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    @IBOutlet var resultLabel5: UILabel!
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    var password = Int(arc4random_uniform(100000))
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        resultLabel1.text = "?"
+        resultLabel2.text = "?"
+        resultLabel3.text = "?"
+        resultLabel4.text = "?"
+
+        countLabel.text = "0"
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +39,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func start(){
+        for i in 0 ..< 100000 {
+            countLabel.text = String(i)
+            
+            NSRunLoop.mainRunLoop().runUntilDate(NSDate(timeIntervalSinceNow: 0.000000000000000000000000000000000000000000000000001))
+            
+            if i == password {
+                var digits = [Int]()
+                
+                for j in 0 ..< 5 {
+                    digits.append(password % 10)
+                    
+                    password /= 10
+                }
+            
+            resultLabel1.text = String(digits[0])
+            resultLabel2.text = String(digits[1])
+            resultLabel3.text = String(digits[2])
+            resultLabel4.text = String(digits[3])
+            resultLabel4.text = String(digits[4])
+  
+            }
+        }
+    }
+    
 
 }
 
